feec0bd9789b16877a8999eb7fbabf7a43b9a0fc
William Gao <williamgao@MacBookPro.hsd1.ma.comcast.net>
2025-07-02 15:18:24 -0400

    changes

---
+++ workspace-addon/src/Constants.ts
@@ -0,0 +1,8 @@
+const CONFIG = (() => {
+    return {
+      APP_NAME: 'Frootful',
+      LOGIN_URL: 'https://frootful.io/login',
+      SUPABASE_URL: 'https://zkglvdfppodwlgzhfgqs.supabase.co',
+      SUPABASE_ANON_KEY: 'your-supabase-key-here'
+    };
+  })();
